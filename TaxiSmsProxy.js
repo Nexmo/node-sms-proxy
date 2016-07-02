a71f811e086cc0aceb5edad78e9d2d23743e9f11
leggetter <phil@leggetter.co.uk>
2016-07-02 02:07:57 +0100

    journeys -> bookings Better terminology

--- TaxiSmsProxy.js
+++ TaxiSmsProxy.js
@@ -1,13 +1,17 @@
 var NEXMO_API_KEY = process.env.NEXMO_API_KEY;
 var NEXMO_API_SECRET = process.env.NEXMO_API_SECRET;
 var NEXMO_DEBUG = process.env.NEXMO_DEBUG;
+var SMS_WEBHOOK_URL = process.env.SMS_WEBHOOK_URL;
 
 var Nexmo = require('nexmo');
 var nexmo = new Nexmo({apiKey: NEXMO_API_KEY, apiSecret: NEXMO_API_SECRET}, {debug: NEXMO_DEBUG});
 
 var TaxiSmsProxy = function() {
+  // LVNs to be assigned to passenger and driver
   this.provisionedNumbers = ['447520615084', '447520619555'];
-  this.activeJourneys = [];
+  
+  // In progress bookings
+  this.bookings = [];
 };
 
 TaxiSmsProxy.prototype.provisionLVNs = function() {
@@ -35,17 +39,28 @@ TaxiSmsProxy.prototype.buyNumber = function(number) {
       console.error(err)
     }
     else {
+      this.configureNumber(number);
+    }
+  }.bind(this));
+};
+
+TaxiSmsProxy.prototype.configureNumber = function(number) {
+  nexmo.number.update(number.country, number.msisdn, {moHttpUrl: SMS_WEBHOOK_URL}, function(err, res) {
+    if(err) {
+      console.error(err)
+    }
+    else {
       this.provisionedNumbers.push(number.msisdn);
     }
   }.bind(this));
 };
 
-TaxiSmsProxy.prototype.startJourney = function(passengerRealNumber, driverRealNumber) {
-  // Create journey object
+TaxiSmsProxy.prototype.makeBooking = function(passengerRealNumber, driverRealNumber) {
+  // Create booking object
   // For demo purposes:
   // - Use first indexed LVN for passenger
   // - Use second indexed LVN for driver
-  var journey = {
+  var booking = {
       passenger: {
         realNumber: passengerRealNumber,
         lvn: this.provisionedNumbers[0],
@@ -55,44 +70,45 @@ TaxiSmsProxy.prototype.startJourney = function(passengerRealNumber, driverRealNu
         realNumber: driverRealNumber,
         lvn: this.provisionedNumbers[1],
         name: 'Driver'
-      }
+      },
+      messages: []
   };
   
-  this.activeJourneys.push(journey);
+  this.bookings.push(booking);
 };
 
-var fromPassengerToDriver = function(from, to, journey) {
+var fromPassengerToDriver = function(from, to, booking) {
   console.log('fromPassengerToDriver',
-    from, '===', journey.passenger.realNumber,
-    to, '===', journey.driver.lvn
+    from, '===', booking.passenger.realNumber,
+    to, '===', booking.driver.lvn
   );
-  return (from === journey.passenger.realNumber &&
-          to == journey.driver.lvn);
+  return (from === booking.passenger.realNumber &&
+          to == booking.driver.lvn);
 };
-var fromDriverToPassenger = function(from, to, journey) {
+var fromDriverToPassenger = function(from, to, booking) {
   console.log('fromDriverToPassenger',
-    from, '===', journey.driver.realNumber,
-    to, '===', journey.passenger.lvn
+    from, '===', booking.driver.realNumber,
+    to, '===', booking.passenger.lvn
   );
-  return (from === journey.driver.realNumber &&
-          to === journey.passenger.lvn);
+  return (from === booking.driver.realNumber &&
+          to === booking.passenger.lvn);
 };
 
 TaxiSmsProxy.prototype.getProxyRoute = function(from, to) {  
   var proxyRoute = null;
-  var journey;
-  for(var i = 0, l = this.activeJourneys.length; i < l; ++i) {
-    journey = this.activeJourneys[i];
+  var booking;
+  for(var i = 0, l = this.bookings.length; i < l; ++i) {
+    booking = this.bookings[i];
     
-    // Use to and from to determine the active journey
-    var toDriver = fromPassengerToDriver(from, to, journey);
-    var toPassenger = fromDriverToPassenger(from, to, journey);
+    // Use to and from to determine the booking
+    var toDriver = fromPassengerToDriver(from, to, booking);
+    var toPassenger = fromDriverToPassenger(from, to, booking);
     
     if(toDriver || toPassenger) {
       proxyRoute = {
-        journey: journey,
-        to: toDriver? journey.driver : journey.passenger,
-        from: toDriver? journey.passenger : journey.driver
+        booking: booking,
+        to: toDriver? booking.driver : booking.passenger,
+        from: toDriver? booking.passenger : booking.driver
       };
       break;
     }
@@ -106,11 +122,18 @@ TaxiSmsProxy.prototype.proxySms = function(from, to, message) {
   var proxyRoute = this.getProxyRoute(from, to);
   
   if(proxyRoute === null) {
-    var errorText = 'No active journey' +
+    var errorText = 'No booking found' +
                     ' from: ' + from +
                     ' to: ' + to;
     throw new Error(errorText);
   }
+  
+  // Store the message
+  proxyRoute.booking.messages.push({
+    from: from,
+    to: to,
+    message: message
+  });
   
   // Always send
   // - from the LVN

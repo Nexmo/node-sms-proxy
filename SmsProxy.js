636ccf059a501756b234029744fc6d8889cbb8f8
Mark Lewin <mark.lewin@vonage.com>
2018-12-17 14:52:29 +0000

    Simplify tutorial

--- SmsProxy.js
+++ SmsProxy.js
@@ -1,229 +1,105 @@
-var Nexmo = require('nexmo');
-var Promise = require('bluebird');
+'use strict'
 
-/**
- * Create a new SmsProxy
- */
-var SmsProxy = function(config) {
-  this.config = config;
-  
-  this.nexmo = new Nexmo({
-      apiKey: this.config.NEXMO_API_KEY, 
-      apiSecret: this.config.NEXMO_API_SECRET
-    },{
-      debug: this.config.NEXMO_DEBUG
-    });
-  
-  // Virtual Numbers to be assigned to UserA and UserB
-  this.provisionedNumbers = [].concat(this.config.PROVISIONED_NUMBERS);
-  
-  // In progress conversations
-  this.conversations = [];
-};
+const Nexmo = require('nexmo');
 
-/**
- * Provision two virtual numbers. Would provision more in a real app.
- */
-SmsProxy.prototype.provisionVirtualNumbers = function() {
-  // Buy a UK number with SMS capabilities.
-  this.nexmo.number.search('GB', {features: 'SMS'}, function(err, res) {
-    if(err) {
-      console.error(err);
+class SmsProxy {
+
+    /**
+     * Create a new SmsProxy
+     */
+    constructor(config) {
+
+        this.config = config;
+
+        this.nexmo = new Nexmo({
+            apiKey: this.config.NEXMO_API_KEY,
+            apiSecret: this.config.NEXMO_API_SECRET
+        }, {
+                debug: true
+            });
     }
-    else {
-      var numbers = res.numbers;
-      
-      // For demo purposes:
-      // - Assume that at least two numbers will be available
-      // - Rent just two virtual numbers: one for each conversation participant
-      this.rentNumber(numbers[0]);
-      this.rentNumber(numbers[1]);
+
+    createConversation(userANumber, userBNumber) {
+        this.conversation = {
+            userA: {
+                realNumber: userANumber,
+                virtualNumber: this.config.VIRTUAL_NUMBER_A
+            },
+            userB: {
+                realNumber: userBNumber,
+                virtualNumber: this.config.VIRTUAL_NUMBER_B
+            }
+        };
+
+        this.sendSMS();
     }
-  }.bind(this));
-};
 
-/**
- * Rent the given number
- */
-SmsProxy.prototype.rentNumber = function(number) {
-  this.nexmo.number.buy(number.country, number.msisdn, function(err, res) {
-    if(err) {
-      console.error(err);
+    sendSMS() {
+        // Send UserA conversation information
+        // From the UserB virtual number
+        // To the UserA real number
+        console.log(this.conversation);
+
+        this.nexmo.message.sendSms(this.conversation.userB.virtualNumber,
+            this.conversation.userA.realNumber,
+            'Reply to this SMS to talk to UserB');
+
+        // Send UserB conversation information
+        // From the UserA virtual number
+        // To the UserB real number
+        this.nexmo.message.sendSms(this.conversation.userA.virtualNumber,
+            this.conversation.userB.realNumber,
+            'Reply to this SMS to talk to UserA');
     }
-    else {
-      this.configureNumber(number);
+
+    fromUserAToUserB(from, to, conversation) {
+        return (from === this.conversation.userA.realNumber &&
+            to === this.conversation.userB.virtualNumber);
     }
-  }.bind(this));
-};
 
-/**
- * Check if the supplied number has already been stored as provisioned.
- */
-SmsProxy.prototype.isProvisioned = function(checkNumber) {
-  this.provisionedNumbers.forEach(function(number) {
-    if(checkNumber.msisdn === number.msisdn) {
-      return true;
+    fromUserBToUserA(from, to, conversation) {
+        return (from === this.conversation.userB.realNumber &&
+            to === this.conversation.userA.virtualNumber);
     }
-  });
-  return false;
-};
 
-/**
- * Configure the number to be associated with the SMS Proxy application.
- */
-SmsProxy.prototype.configureNumber = function(number) {
-  this.nexmo.number.update(number.country, number.msisdn, {moHttpUrl: this.config.SMS_WEBHOOK_URL}, function(err, res) {
-    if(err) {
-      console.error(err);
+    getProxyRoute(from, to) {
+        let proxyRoute = null;
+
+            // Use to and from numbers to work out who is sending to whom
+            const fromUserA = this.fromUserAToUserB(from, to, this.conversation);
+            const fromUserB = this.fromUserBToUserA(from, to, this.conversation);
+
+            if (fromUserA || fromUserB) {
+                proxyRoute = {
+                    to: fromUserA ? this.conversation.userB : this.conversation.userA,
+                    from: fromUserA ? this.conversation.userA : this.conversation.userB
+                };
+                //break;
+            }
+        
+
+        return proxyRoute;
     }
-    else if(!this.isProvisioned(number)) {
-      this.provisionedNumbers.push(number);
+
+    proxySms(from, to, text) {
+        // Determine how the SMS should be routed
+        const proxyRoute = this.getProxyRoute(from, to);
+        console.log(proxyRoute)
+
+        if (proxyRoute === null) {
+            const errorText = 'No conversation found' +
+                ' from: ' + from +
+                ' to: ' + to;
+            throw new Error(errorText);
+        }
+
+        // Always send
+        // - from the virtual number
+        // - to the real number
+        this.nexmo.message.sendSms(proxyRoute.from.virtualNumber,
+            proxyRoute.to.realNumber, text);
     }
-  }.bind(this));
-};
 
-/**
- * Ensure the existing provisioned numbers are linked to the SMS Proxy app.
- */
-SmsProxy.prototype.reconfigureNumbers = function() {
-  this.provisionedNumbers.forEach(this.configureNumber.bind(this));
-};
-
-/**
- * Create a new tracked conversation so there is a real/virtual mapping of numbers.
- */
-SmsProxy.prototype.createConversation = function(userANumber, userBNumber, cb) {
-  this.checkNumbers(userANumber, userBNumber)
-    .then(this.saveConversation.bind(this))
-    .then(this.sendSMS.bind(this))
-    .then(function(conversation) {
-      cb(null, conversation);
-    })
-    .catch(function(err) {
-      console.error(err);
-      cb(err);
-    });
-};
-
-/**
- * Ensure the given numbers are valid and which country they are associated with.
- */
-SmsProxy.prototype.checkNumbers = function(userANumber, userBNumber) {
-  var niGetPromise = Promise.promisify(this.nexmo.numberInsight.get, {context: this.nexmo.numberInsight});
-  var userAGet = niGetPromise({level: 'basic', number: userANumber});
-  var userBGet = niGetPromise({level: 'basic', number: userBNumber});
-  
-  return Promise.all([userAGet, userBGet]);
-};
-
-/**
- * Store the conversation information.
- */
-SmsProxy.prototype.saveConversation = function(results) {
-  var userAResult = results[0];
-  var userANumber = {
-    msisdn: userAResult.international_format_number,
-    country: userAResult.country_code
-  };
-  
-  var userBResult = results[1];
-  var userBNumber = {
-    msisdn: userBResult.international_format_number,
-    country: userBResult.country_code
-  };
-  
-  // Create conversation object - for demo purposes:
-  // - Use first indexed LVN for user A
-  // - Use second indexed LVN for user B
-  var conversation = {
-    userA: {
-      realNumber: userANumber,
-      virtualNumber: this.provisionedNumbers[0]
-    },
-    userB: {
-      realNumber: userBNumber,
-      virtualNumber: this.provisionedNumbers[1]
-    }
-  };
-  
-  this.conversations.push(conversation);
-  
-  return conversation;
-};
-
-/**
- * Send an SMS to each conversation participant so they know each other's
- * virtual number and can SMS each other via the proxy.
- */
-SmsProxy.prototype.sendSMS = function(conversation) {
-  // Send UserA conversation information
-  // From the UserB virtual number
-  // To the UserA real number
-  this.nexmo.message.sendSms(conversation.userB.virtualNumber.msisdn,
-                             conversation.userA.realNumber.msisdn,
-                             'Reply to this SMS to talk to UserB');
-
-  // Send UserB conversation information
-  // From the UserA virtual number
-  // To the UserB real number
-  this.nexmo.message.sendSms(conversation.userA.virtualNumber.msisdn,
-                             conversation.userB.realNumber.msisdn,
-                             'Reply to this SMS to talk to UserA');
-                        
-  return conversation;
-};
-
-var fromUserAToUserB = function(from, to, conversation) {
-  return (from === conversation.userA.realNumber.msisdn &&
-          to === conversation.userB.virtualNumber.msisdn);
-};
-var fromUserBToUserA = function(from, to, conversation) {
-  return (from === conversation.userB.realNumber.msisdn &&
-          to === conversation.userA.virtualNumber.msisdn);
-};
-
-/**
- * Work out real number to virual number mapping between users.
- */
-SmsProxy.prototype.getProxyRoute = function(from, to) { 
-  var proxyRoute = null;
-  var conversation;
-  for(var i = 0, l = this.conversations.length; i < l; ++i) {
-    conversation = this.conversations[i];
-    
-    // Use to and from to determine the conversation
-    var fromUserA = fromUserAToUserB(from, to, conversation);
-    var fromUserB = fromUserBToUserA(from, to, conversation);
-    
-    if(fromUserA || fromUserB) {
-      proxyRoute = {
-        to: fromUserA? conversation.userB : conversation.userA,
-        from: fromUserA? conversation.userA : conversation.userB
-      };
-      break;
-    }
-  }
-  
-  return proxyRoute;
-};
-
-SmsProxy.prototype.proxySms = function(from, to, text) {  
-  // Determine how the SMS should be routed
-  var proxyRoute = this.getProxyRoute(from, to);
-  
-  if(proxyRoute === null) {
-    var errorText = 'No booking found' +
-                    ' from: ' + from +
-                    ' to: ' + to;
-    throw new Error(errorText);
-  }
-  
-  // Always send
-  // - from the virtual number
-  // - to the real number
-  this.nexmo.message.sendSms(proxyRoute.from.virtualNumber.msisdn,
-                             proxyRoute.to.realNumber.msisdn,
-                             text);
-};
+}
 
 module.exports = SmsProxy;
--- server.js
+++ server.js
@@ -1,66 +1,50 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const SmsProxy = require('./SmsProxy');
 
-var app = express();
-app.set('port', (process.env.PORT || 5000));
+const app = express();
+app.set('port', (process.env.PORT || 3000));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(app.get('port'), function() {
-  console.log('SMS Proxy App listening on port', app.get('port'));
+app.listen(app.get('port'), function () {
+    console.log('SMS Proxy App listening on port', app.get('port'));
 });
 
-require('dotenv').config();
-var config = require(__dirname + '/../config');
+const config = require('dotenv').config();
 
-var SmsProxy = require('./SmsProxy');
-var smsProxy = new SmsProxy(config);
+const smsProxy = new SmsProxy(config.parsed);
 
-app.post('/inbound-sms', function(req, res) {
-  var from = req.body.msisdn;
-  var to = req.body.to;
-  var text = req.body.text;
-  
-  smsProxy.proxySms(from, to, text);
-  
-  res.sendStatus(200);
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
+
+// Handle and route incoming SMS to virtual numbers
+app.post('/webhooks/inbound-sms', (req, res) => {
+    var from = req.body.msisdn;
+    var to = req.body.to;
+    var text = req.body.text;
+
+    console.log(`from ${from} to ${to} - ${text}`)
+
+    // Route virtual number to real number
+    smsProxy.proxySms(from, to, text);
+
+    res.sendStatus(200);
 });
 
-// Useful functions for testing out the functionality and querying bookings
-app.post('/conversation', function(req, res) {
-  var userANumber = req.body.userANumber;
-  var userBNumber = req.body.userBNumber;
-  
-  smsProxy.createConversation(userANumber, userBNumber, function(err, result) {
-    if(err) {
-      res.status(500).json(err);
-    }
-    else {
-      res.json(result);
-    }
-  });
-  
+// Start a conversation
+app.post('/conversation', (req, res) => {
+    var userANumber = req.body.userANumber;
+    var userBNumber = req.body.userBNumber;
+
+    smsProxy.createConversation(userANumber, userBNumber, (err, result) => {
+        if (err) {
+            res.status(500).json(err);
+        }
+        else {
+            res.json(result);
+        }
+    });
+
 });
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
-app.get('/provision', function(req, res) {
-  smsProxy.provisionLVNs();
-  
-  res.sendStatus(200);
-});
-
-app.get('/configure-numbers', function(req, res) {
-  smsProxy.reconfigureNumbers();
-  
-  res.sendStatus(200);
-});
-
-app.get('/provisioned', function(req, res) {
-  res.json(smsProxy.provisionedNumbers);
-});
-
-app.get('/conversations', function(req, res) {
-  res.json(smsProxy.conversations);
-});
